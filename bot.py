ea426f5854133feccc3048dad4bdaa6fe88cdbbd
jkyle109 <jkyle109@gmail.com>
2021-03-03 07:59:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,37 @@
 import slack
 import os
 import json
-import requests
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response, render_template
+from flask import Flask
 from slackeventsapi import SlackEventAdapter
+import requests
 
-
-# Load the Token from .env file
+# LOADING ENVIRONMENT
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# WEB SERVER
+app = Flask(__name__)
 PORT = int(os.environ.get('PORT', 6969))
 HOST = os.environ.get('HOST', None)
-SIGNING_SECRET = os.environ['SIGNING_SECRET']
-SLACK_TOKEN = os.environ['SLACK_TOKEN']
-WEBHOOK_URL = os.environ['WEBHOOK_URL']
-apiKey = os.environ['API_KEY']
-
-app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(
-    SIGNING_SECRET, '/slack/events', app)
+    os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-# Using WebClient in slack, there are other clients built-in as well !!
-client = slack.WebClient(token=SLACK_TOKEN)
-
-# connect the bot to the channel in Slack Channel
-client.chat_postMessage(channel='#jons-hideout', text="I'm Ready!")
-# test_data = {'text': 'I\'m Ready!'}
-# requests.post(WEBHOOK_URL, json.dumps(test_data))
-
-BOT_ID = client.api_call('auth.test')['user_id']
-
-
-message_counter = {}
+# STORING TOKEN
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+apiKey = os.environ['API_KEY']
 
 
 @slack_event_adapter.on('message')
 def message(payload):
-    # print(payload)
-    event = payload.get('event')
+    event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    if BOT_ID != user_id:
-        if user_id in message_counter:
-            message_counter[user_id] += 1
-        else:
-            message_counter[user_id] = 1
 
+    if BOT_ID != user_id:
         if text[-1] == '?':
             client.chat_postMessage(channel=channel_id, text=text)
         else:
@@ -104,17 +86,5 @@ def message(payload):
                     channel=channel_id, attachments=message)
 
 
-@app.route('/mc', methods=['POST'])
-def message_count():
-    data = request.form
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-    count = message_counter.get(user_id, 0)
-
-    client.chat_postMessage(
-        channel=channel_id, text=f"You have {count} message(s).")
-    return Response(), 200
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=PORT, host=HOST)
